303f3269b9f41fb687e21b0c790b9ac340de2893
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2015-12-02 00:21:16 +0530

    Defined of onCreateView(). Partial implementation of method to query paired devices

--- SignalSender/app/src/main/java/com/example/classiclogic/signalsender/BluetoothConnectActivityFragment.java
+++ SignalSender/app/src/main/java/com/example/classiclogic/signalsender/BluetoothConnectActivityFragment.java
@@ -2,6 +2,7 @@ package com.example.classiclogic.signalsender;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -9,6 +10,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.Set;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -22,12 +26,6 @@ public class BluetoothConnectActivityFragment extends Fragment {
     CommunicationInterface communicationCallback;
 
     public BluetoothConnectActivityFragment() {
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bluetooth_connect, container, false);
     }
 
     @Override
@@ -47,7 +45,31 @@ public class BluetoothConnectActivityFragment extends Fragment {
         }
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bluetooth_connect, container, false);
+    }
+
+
     public interface CommunicationInterface {
         void getBluetoothAdapter(BluetoothAdapter bluetoothAdapter);
+    }
+
+    private ArrayAdapter<BluetoothDevice> getPairedDevices(BluetoothAdapter btAdapter)  {
+        ArrayAdapter<BluetoothDevice> mArrayAdapter = new ArrayAdapter<BluetoothDevice>(this, )
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+
+        for(BluetoothDevice device: pairedDevices)  {
+
+        }
+
     }
 }
